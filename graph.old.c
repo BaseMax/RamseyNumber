b11f7ab767b9f92b118463f2de569aa332dcf32f
Max Base <MaxBaseCode@Gmail.Com>
2019-07-29 20:33:30 +0430

    Update graph.old.c

--- graph.old.c
+++ graph.old.c
@@ -1,4 +1,13 @@
-// https://github.com/BaseMax/
+/**
+ *
+ * @Name : RamseyNumber/graph.old.c
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-07-15
+ * @Released under : https://github.com/BaseMax/RamseyNumber/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/RamseyNumber
+ *
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
