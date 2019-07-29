9753085ff98ed4c5e7fadf97aa870b05dc01fafa
Max Base <MaxBaseCode@Gmail.Com>
2019-07-29 20:33:09 +0430

    Update graph.test.c

--- graph.test.c
+++ graph.test.c
@@ -1,3 +1,13 @@
+/**
+ *
+ * @Name : RamseyNumber/graph.test.c
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-07-15
+ * @Released under : https://github.com/BaseMax/RamseyNumber/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/RamseyNumber
+ *
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
